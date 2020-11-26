75482246c4bd90c195a59bca0dd0fa3d8f5c31b0
EeshaArif <43031234+EeshaArif@users.noreply.github.com>
2020-11-26 09:54:31 +0500

    feat: implement touch command (#21)
    check issue for details
    
    BREAKING CHANGE: renamed get_files_From_directories to get-files_from_directory in files_Dao
    
    Closes #13

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from commands.ping import PingCommand
 from commands.cd import CDCommand
 from commands.ls import LSCommand
 from commands.mkdir import MkDirCommand
+from commands.touch import TouchCommand
 
 from db.db import DB
 
@@ -16,7 +17,7 @@ def main():
     db.connect(os.path.join(os.getcwd(), 'finder.db'))
 
     terminal = BaseTerminal(commands=[
-        PingCommand, CDCommand, MkDirCommand, LSCommand
+        PingCommand, CDCommand, MkDirCommand, LSCommand, TouchCommand
     ])
     terminal.run()
 
