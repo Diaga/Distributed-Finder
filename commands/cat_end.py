dbf9921ff4677f2eb8784bd7f80e688c7619145b
EeshaArif <eeshaarif@gmail.com>
2020-11-27 03:42:32 +0500

    Update cat_end command for parsed entries

--- commands/cat_end.py
+++ commands/cat_end.py
@@ -1,6 +1,7 @@
 from base.command import BaseCommand
 from base.arguments import StringArgument
 from db.dao.file_dao import FileDao
+from db.dao.directory_dao import DirectoryDao
 
 
 class CatEndCommand(BaseCommand):
@@ -10,9 +11,10 @@ class CatEndCommand(BaseCommand):
     def run(self):
         path = self.arguments[0].data
         file = self.context.parse(path, True)
-        current_directory = self.context.current_directory
+        file_directory = DirectoryDao.get_directory_from_directory_id(
+            file.directory_id)
 
-        if (FileDao.is_unique_filename(file.name, current_directory)
+        if (FileDao.is_unique_filename(file.name, file_directory)
                 or not FileDao.is_valid_filename(file.name)):
             raise ValueError('InvalidEntry: File does not exist')
         else:
