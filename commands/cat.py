92772635672e873a0beb1162da7e7350d7334a23
EeshaArif <43031234+EeshaArif@users.noreply.github.com>
2020-11-27 15:59:22 +0500

    :feat: Implement and update cat commands (#36)
    * update rm functionailty
    
    * upadate cat command for parsed entries
    
    * Update cat_end command for parsed entries
    
    * Resolve required changes

--- commands/cat.py
+++ commands/cat.py
@@ -3,12 +3,14 @@ from base.arguments import StringArgument
 from db.dao.file_dao import FileDao
 
 
-class CatEndCommand(BaseCommand):
-    command = 'cat-end'
+class CatCommand(BaseCommand):
+    command = 'cat'
     arguments = [StringArgument()]
 
     def run(self):
         path = self.arguments[0].data
         file = self.context.parse(path, True)
+        self.log('WARNING: Data will be overwritten', prefix=False)
+        FileDao.remove_data_in_file(file)
         text = self.get_input('Start Writing: ', prefix=False)
         FileDao.insert_data_in_file(file, text)
