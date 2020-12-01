907e4690f68dcaafd1a8aab9db412024ca061864
syedbabart <58231237+syedbabart@users.noreply.github.com>
2020-12-01 21:46:44 +0500

    :fix: update ls command
    * Update ls.py
    
    * Update ls.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ls.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ls.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ls.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ls.py
    
    * Update ls.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ls.py
    
    * Update ls.py
    
    * Update ls.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- commands/ls.py
+++ commands/ls.py
@@ -8,9 +8,29 @@ class LSCommand(BaseCommand):
     command = 'ls'
 
     def run(self):
-        self.log(DirectoryDao.get_directories_from_current_directory(
-            self.context.current_directory), prefix=False
-        )
-        # logging files - temp
-        self.log(FileDao.get_files_from_current_directory(
-            self.context.current_directory), prefix=False)
+        def display_lists(dir_list):
+            list_dirlist = list(dir_list)
+            for i in range((len(list_dirlist)) - 1):
+                list_dirlist[i] = str(list_dirlist[i])
+            for i in range((len(list_dirlist)) - 1):
+                list_dirlist[i] += '       '
+            n = m = 0
+            x = len(list_dirlist)
+            while m < x:
+                m = m+3
+                if ((x-1) >= (n+2)):
+                    print(list_dirlist[n], list_dirlist[n+1],
+                          list_dirlist[n+2])
+                elif((x-1) < (n+2) and (x-1) >= (n+1)):
+                    print(list_dirlist[n], list_dirlist[n+1])
+                else:
+                    print(list_dirlist[n])
+                n = m
+        dir_list = DirectoryDao.get_directories_from_current_directory(
+                self.context.current_directory)
+        file_list = FileDao.get_files_from_current_directory(
+            self.context.current_directory)
+        self.log('Directories: ')
+        display_lists(dir_list)
+        self.log('Files: ', prefix=False)
+        display_lists(file_list)
