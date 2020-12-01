def13f175d2dd5e87226893a2da2f92a25e4cd94
syedbabart <58231237+syedbabart@users.noreply.github.com>
2020-12-01 21:11:58 +0500

    Update ls.py

--- commands/ls.py
+++ commands/ls.py
@@ -8,9 +8,27 @@ class LSCommand(BaseCommand):
     command = 'ls'
 
     def run(self):
-        self.log(DirectoryDao.get_directories_from_current_directory(
-            self.context.current_directory), prefix=False
-        )
-        # logging files - temp
-        self.log(FileDao.get_files_from_current_directory(
-            self.context.current_directory), prefix=False)
+        def display_lists(dir_list):
+            list_dirlist=list(dir_list)
+            for i in range((len(list_dirlist)) - 1):
+                list_dirlist[i]=str(list_dirlist[i])
+            for i in range((len(list_dirlist)) - 1):
+                list_dirlist[i]+='       '
+            n = m = 0
+            while m < len(list_dirlist):
+                m = m+3
+                if ((len(list_dirlist)-1)>=(n+2)):
+                    print(list_dirlist[n], list_dirlist[n+1], list_dirlist[n+2])
+                elif((len(list_dirlist)-1)<(n+2) and (len(list_dirlist)-1)>=(n+1)):
+                    print(list_dirlist[n], list_dirlist[n+1])
+                else:
+                    print(list_dirlist[n])
+                n = m
+        dir_list=DirectoryDao.get_directories_from_current_directory(
+                self.context.current_directory)
+        file_list=FileDao.get_files_from_current_directory(
+            self.context.current_directory)
+        self.log('Directories: ')
+        display_lists(dir_list)
+        self.log('Files: ', prefix=False)
+        display_lists(file_list)
